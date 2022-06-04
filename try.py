7ebb58a0f95e608da4f51eb555e7e7d43a031b3b
Stefan Siegler <stefan@siegler.ch>
2022-06-04 18:20:50 +0200

    v0.9.2

--- try.py
+++
@@ -1,8 +0,0 @@
-from log import Logging
-log = Logging('test', file_name='test.log', file_mode='w', logging_level='DEBUG', console_output=True)
-
-log.debug('hallo')
-log.info('das')
-log.warning('ist')
-log.error('ein')
-log.critical('test')
